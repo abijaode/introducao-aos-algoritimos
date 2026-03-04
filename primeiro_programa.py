46e106d99388a03bc1c9d50bc51e148d34a56017
Lucas Abijaode <lucasabijaode06@gmail.com>
2026-03-04 11:53:49 -0300

    Replace initial greeting with a program to calculate average of two grades

--- primeiro_programa.py
+++ primeiro_programa.py
@@ -1,1 +1,6 @@
-print("Olá mundo, sou o Lucas!")
+print(f"Digite a primeira nota:")
+nota1 = float(input())
+print(f"Digite a segunda nota:")
+nota2 = float(input())
+media = (nota1 + nota2) / 2
+print(f"A sua média é: {media}")
