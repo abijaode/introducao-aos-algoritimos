13cd4693825d646f6f26ab123bbaa87ac5f887f3
Lucas Abijaode <lucasabijaode06@gmail.com>
2026-03-18 11:38:48 -0300

    atividades feitas em sala de aula

---
+++ aulas/exercicio_1_B.py
@@ -0,0 +1,17 @@
+# Escreva um programa que leia um número inteiro informado pelo usuário.
+# O programa deve verificar se o número está entre 10 e 20.
+# Se o número estiver dentro desse intervalo, mostre:
+# Número dentro do intervalo
+# Caso contrário, mostre:
+# Número fora do intervalo
+# Desafio adicional
+# Versão A: utilizando dois if separados para verificar os limites do intervalo.
+# Versão B: utilizando um único if com operador lógico and.
+
+numero = int(input("Digite o seu número: "))
+
+if numero > 10 and numero < 20:
+        print("Numero dentro do intervalo: ", numero)
+else: 
+        print("Numero fora do intervalo: ", numero)
+
---
+++ aulas/exercicio_2_A.py
@@ -0,0 +1,16 @@
+# Escreva um programa que leia a temperatura atual em graus Celsius.
+# O programa deve verificar as seguintes situações:
+# se a temperatura é menor que 10
+# se a temperatura está entre 10 e 25
+# se a temperatura é maior que 25
+# Para cada situação, o programa deve mostrar:
+# Temperatura baixa
+# Temperatura agradável
+# Temperatura alta
+# Desafio adicional
+# Versão A: utilizando vários if independentes.
+# Versão B: utilizando operadores lógicos (and) para verificar intervalos, e exclusão mútua.
+
+
+temperatura = int(input("Digite a temperatura em Celsius: "))
+
