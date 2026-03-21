b6cf08360f6510e42966096409d2bbcb17f8936f
Lucas <lucasabijaode06@gmail.com>
2026-03-21 14:38:09 -0300

    novas atividades e correção na questao 5

---
+++ aulas/exercicio_5_B.py
@@ -0,0 +1,17 @@
+# Uma disciplina utiliza dois critérios para aprovação:
+# média final
+# frequência
+# O programa deve ler:
+# média do aluno
+# percentual de frequência
+# As regras são:
+# Se a frequência for menor que 75%, o aluno está reprovado por falta.
+# Caso contrário, analisar a média:
+# média ≥ 60 → aprovado
+# média entre 40 e 59 → recuperação
+# média abaixo de 40 → reprovado por nota
+# O programa deve mostrar o resultado final do aluno.
+# Desafio adicional
+# Versão A: utilizando condicionais aninhadas.
+# Versão B: tentando reduzir o número de níveis de aninhamento usando operadores lógicos (and).
+
